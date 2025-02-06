8ab62f2763f3b56c757c7ff52ed24faffcef21ae
Tooooom-fcgb <tom.detaevernier@mmibordeaux.com>
2025-02-06 11:16:17 +0100

    le truc de pops

--- footer.php
+++ footer.php
@@ -1,8 +1,13 @@
 <!-- Load JS scripts -->
 
+	</main>
+	<footer>
+		<section class="footer">
+			<img class="footer-image" src="/src/images/footer-image.png">
+		</section>
+	</footer>
 </body>
-<footer>
-	<!-- footer -->
-
-</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.js"></script>
+</html>
 </html>
--- header.php
+++ header.php
@@ -23,17 +23,26 @@ if (isset($_SESSION['pseudoMemb'])) {
 <html lang="fr-FR">
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Blog'Art</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bordeaux en Résistance - Accueil</title>
     <!-- Load CSS -->
     <link rel="stylesheet" href="src/css/style.css" />
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/index-style.css">
+    <link rel="stylesheet" href="css/font.css">
+    <link rel="stylesheet" href="./Bo">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.css">
+    <link rel="icon" type="image/png" href="src/svg/favicon.svg">
     <!-- Bootstrap CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
     <link rel="shortcut icon" type="image/x-icon" href="src/images/article.png" />
 </head>
 <?php
 //load config
 require_once 'config.php';
+
+/*
 ?>
 <body>
 <nav class="navbar navbar-expand-lg bg-light">
@@ -81,4 +90,55 @@ require_once 'config.php';
     </div>
   </div>
 </nav>
+*/
 
+
+?>
+<body>
+  <header>
+        <section class="menu">
+            <div class="header-left">
+                <a href="./index.html"><img class="logo" src="src/svg/logo3.svg"></a>
+                <div class="nav">
+                    <a href="./index.html">ACCUEIL</a>
+                    <a href="./articles.html">ARTICLES</a>
+                    <?php  
+                    if ($is_admin) {
+                    echo '<a class="nav-link" href="/views/backend/dashboard.php">Admin</a>';
+                    } ?>
+                </div>
+            </div>
+            <div class="header-right">
+                <div class="search-container">
+                    <div class="collapse" id="searchCollapse">
+                        <input type="text" class="custom-form-search search-bar" placeholder="Rechercher...">
+                    </div>
+                    <a data-bs-toggle="collapse" href="#searchCollapse" role="button" aria-expanded="false" aria-controls="searchCollapse">
+                        <img src="src/svg/loupe.svg" class="search-icon" alt="Recherche">
+                    </a>
+                </div>
+                <?php 
+
+                if(isset($_SESSION['pseudoMemb']) ){
+
+                echo '<a class="btn btn-danger m-1" href="/api/security/disconnect.php" role="button">Se déconnecter</a>'; 
+                } else { 
+                echo '<a class="btn btn-primary m-1" href="/views/backend/security/login.php" role="button">Login</a>';
+                echo '<a class="btn btn-dark m-1" href="/views/backend/security/signup.php" role="button">Sign up</a>';
+
+                }
+
+                ?>
+                <a href="./login.html">
+                    <div class="bouton">
+                        <span class=bouton>S'identifier</span>
+                        <img class=bouton-image src="src/svg/fleche-bouton.svg">
+                    </div>
+                </a>
+            </div>
+        </section>
+        <div class="ligne">
+            <hr width="90%" color="black">
+        </div>
+    </header> 
+<body>
