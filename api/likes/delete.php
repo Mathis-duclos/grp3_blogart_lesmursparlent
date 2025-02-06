f70d9b403c68944fb1ed802f2efae75f62f31c4e
Mathis Duclos <mathis.duclos@mmibordeaux.com>
2025-02-07 00:44:04 +0100

    LE DELETE IL MARCHE

--- api/likes/delete.php
+++ api/likes/delete.php
@@ -3,14 +3,20 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once '../../functions/ctrlSaisies.php';
 
+// Vérifie que les champs POST existent
+if (isset($_POST['numMemb']) && isset($_POST['numArt'])) {
+    // Récupère et sécurise les entrées
+    $numMemb = ctrlSaisies($_POST['numMemb']);
+    $numArt = ctrlSaisies($_POST['numArt']);
 
-$numMemb = ctrlSaisies($_POST[numMemb[0]]);
-$numArt = ctrlSaisies($_POST[numArt[0]]);
-var_dump($numArt);
-var_dump($numMemb);
+    // var_dump($numMemb, $numArt);
 
+    // Supprime le like de la base de données
+    $result = sql_delete('LIKEART', "numMemb = $numMemb AND numArt = $numArt");
 
-sql_delete('LIKEART', "likeA = 1 AND numMemb =" . $numMemb . "AND numArt =" . $numArt);
-
-header('Location: ../../views/backend/likes/list.php');
-exit();
+    header('Location: ../../views/backend/likes/list.php');
+} else {
+    // Redirige en cas de données manquantes
+    header('Location: ../../views/backend/likes/list.php?error=missing_data');
+    exit();
+}
--- views/backend/likes/delete.php
+++ views/backend/likes/delete.php
@@ -1,6 +1,8 @@
 <?php
 include '../../../header.php';
 
+
+
 // Vérifie et sécurise les entrées GET
 $numMemb = isset($_GET['numMemb']) ? intval($_GET['numMemb']) : 0;
 $numArt = isset($_GET['numArt']) ? intval($_GET['numArt']) : 0;
@@ -12,8 +14,8 @@ $likeStatus = (!empty($estLike) && $estLike[0]['likeA'] == 1) ? "Liké" : "Disli
 // Récupère le titre de l'article
 $plTitre = sql_select('ARTICLE', 'libTitrArt', 'numArt = ' . $numArt);
 $plTitre = !empty($plTitre) ? $plTitre[0]['libTitrArt'] : 'Titre inconnu';
-?>
 
+?>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
@@ -21,18 +23,22 @@ $plTitre = !empty($plTitre) ? $plTitre[0]['libTitrArt'] : 'Titre inconnu';
         </div>
         <div class="col-md-12">
             <!-- Formulaire pour supprimer un like -->
-            <form action="<?php echo htmlspecialchars(ROOT_URL . '/api/likes/delete.php?numArt=' . $numArt); ?>" method="post" enctype="multipart/form-data">
+            <form action="<?php echo (ROOT_URL . '/api/likes/delete.php?numArt=' . $numArt); ?>" method="post" enctype="multipart/form-data">
                 <div>
                     <label for="numArt">Membre ayant liké</label>
-                    <input type="text" class="form-control" id="numArt" name="numArt" value="<?php echo htmlspecialchars($numMemb); ?>" disabled>
+                    <input type="text" class="form-control" id="numArt" name="numArt" value="<?php echo ($numMemb); ?>" disabled>
+                </div>
+                <div class="form-group">
+                    <input type="hidden" id="numMemb" name="numMemb" value="<?php echo ($numMemb); ?>">
+                    <input type="hidden" id="numArt" name="numArt" value="<?php echo ($numArt); ?>">
                 </div>
                 <div class="form-group">
                     <label for="libTitrArt">Nom de l'article liké</label>
-                    <input type="text" class="form-control" id="libTitrArt" name="libTitrArt" value="<?php echo htmlspecialchars($plTitre); ?>" disabled>
+                    <input type="text" class="form-control" id="libTitrArt" name="libTitrArt" value="<?php echo ($plTitre); ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label for="likeStatus">Article liké ou non</label>
-                    <input type="text" class="form-control" id="likeStatus" name="likeStatus" value="<?php echo htmlspecialchars($likeStatus); ?>" disabled>
+                    <input type="text" class="form-control" id="likeStatus" name="likeStatus" value="<?php echo ($likeStatus); ?>" disabled>
                 </div>
                 <button type="submit" class="btn btn-danger">Supprimer</button>
             </form>
