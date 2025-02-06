9c7d52364aedd2d9069f7893a744518d0b2fd48e
Mathis Duclos <mathis.duclos@mmibordeaux.com>
2025-02-06 19:46:42 +0100

    delete like view marche, API pas trop

--- api/likes/delete.php
+++ api/likes/delete.php
@@ -1,2 +1,14 @@
 <?php
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
+
+
+$numMemb = ctrlSaisies($_POST['numMemb']);
+$numArt = ctrlSaisies($_POST['numArt']);
+
+
+sql_delete('LIKEART' , "likeA = 1 AND numMemb =" . $numMemb . "AND numArt =" . $numArt);
+
+header('Location: ../../views/backend/likes/list.php');
+exit();
