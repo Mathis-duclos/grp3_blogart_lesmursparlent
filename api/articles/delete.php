954a50dd70839772aa3005231ae95484d61c2a8f
edouardfort <edouard.fort@mmibordeaux.com>
2025-02-07 23:18:27 +0100

    fix rapide CIR ENCORE

--- api/articles/delete.php
+++ api/articles/delete.php
@@ -8,6 +8,9 @@ $numArt = ctrlSaisies($_GET['numArt']);
 
 sql_delete('MOTCLEARTICLE', "numArt = $numArt");
 sql_delete('ARTICLE', "numArt = $numArt");
+sql_delete('COMMENT', "numArt = $numArt");
+sql_delete('LIKEART', "numArt = $numArt");
+
 
 
 header('Location: ../../views/backend/articles/list.php');
